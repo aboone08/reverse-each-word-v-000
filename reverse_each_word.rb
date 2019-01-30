854e6609c18d084eca32d7f87dee8f54d696502f
Andranique Boone <andranique.boone@gmail.com>
2019-01-30 21:58:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   words=string.split
-  words.collect do |word|
-    word.reverse!
-    return word.to_s
+  words.collect do |words|
+    words.reverse!
+    return words.to_s
   end
 end
