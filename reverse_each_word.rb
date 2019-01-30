460ca2d7cf613d86b01b26dd2ab2a1a5b67e83fd
Andranique Boone <andranique.boone@gmail.com>
2019-01-30 21:29:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = [" "]
 def reverse_each_word(sentence)
   sentence.split.collect do |words|
-    words
-    return sentence.reverse!
+    words.reverse!
+    return sentence.reverse
   end
 end
